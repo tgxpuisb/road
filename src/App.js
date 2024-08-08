25cec245764c48ee55084d93de9097c6eabf0dcf
liyizhi <tgxpuisb@hotmail.com>
2024-08-08 09:16:25 +0800

    axios data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { useEffect } from 'react'
-import './App.css';
-import { Select, Form, Space, Card, Row, Col } from 'antd';
+import { useEffect, useState } from 'react'
+import { Card, Row, Col } from 'antd';
 import Granim from 'granim'
+import axios from 'axios';
+import './App.css';
 
 function App() {
+
+  const [infos, setInfos] = useState([100, 20, 40, 50])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8001/parameters')
+      .then(res => {
+        console.log(res)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }, [])
 
   useEffect(() => {
     new Granim({
@@ -23,85 +37,73 @@ function App() {
       }
     })
 
-    new Granim({
-      element: '#tunnel-2',
-      direction: 'left-right',
-      states : {
-          "default-state": {
-              gradients: [
-                  [
-                    { color: '#00ff00', pos: .2 },
-                    { color: '#00ff00', pos: .5 },
-                    { color: '#ff0000', pos: .8 },
-                    { color: '#00ef7d', pos: 1 }
-                  ],
-              ]
-          }
-      }
-    })
+    // new Granim({
+    //   element: '#tunnel-2',
+    //   direction: 'left-right',
+    //   states : {
+    //       "default-state": {
+    //           gradients: [
+    //               [
+    //                 { color: '#00ff00', pos: .2 },
+    //                 { color: '#00ff00', pos: .5 },
+    //                 { color: '#ff0000', pos: .8 },
+    //                 { color: '#00ef7d', pos: 1 }
+    //               ],
+    //           ]
+    //       }
+    //   }
+    // })
 
-    new Granim({
-      element: '#tunnel-3',
-      direction: 'left-right',
-      states : {
-          "default-state": {
-              gradients: [
-                  [
-                    { color: '#ff3a00', pos: .2 },
-                    { color: '#00ff00', pos: .5 },
-                    { color: '#fd1d1d', pos: .8 },
-                    { color: '#38ef7d', pos: 1 }
-                  ],
-              ]
-          }
-      }
-    })
+    // new Granim({
+    //   element: '#tunnel-3',
+    //   direction: 'left-right',
+    //   states : {
+    //       "default-state": {
+    //           gradients: [
+    //               [
+    //                 { color: '#ff3a00', pos: .2 },
+    //                 { color: '#00ff00', pos: .5 },
+    //                 { color: '#fd1d1d', pos: .8 },
+    //                 { color: '#38ef7d', pos: 1 }
+    //               ],
+    //           ]
+    //       }
+    //   }
+    // })
   }, [])
 
   return (
     <div style={{padding: 24}}>
-      {/* <Form className='filter' style={{ maxWidth: 400 }}>
-        <Form.Item
-          name="tunnel"
-          label="请选择隧道"
-        >
-          <Select
-            defaultValue={'1'}
-            options={[
-              {
-                label: '隧道1',
-                value: '1'
-              },
-              {
-                label: '隧道2',
-                value: '2'
-              },
-            ]}
-          />
-        </Form.Item>
-      </Form> */}
       <Row gutter={24} style={{marginBottom: 24}}>
-        <Col span={8}>
+        <Col span={24}>
           <Card title="流量信息栏">
+            {infos.length > 0 ? 
+              <>
+                <div>隧道内车辆总数：<b>{infos[0]}</b></div>
+                <div>隧道内第一段车辆数：<b>{infos[1]}</b></div>
+                <div>隧道内第二段车辆数：<b>{infos[2]}</b></div>
+                <div>每小时车流量：<b>{infos[3]}</b></div>
+              </>:
+              <span>暂无数据</span>
+            }
+            
           </Card>
         </Col>
-        <Col span={16}>
+        {/* <Col span={16}>
           <Card title="隧道车流量">
-            <canvas className="tunnel" id="tunnel-1"></canvas>
-            <canvas className="tunnel" id="tunnel-2"></canvas>
-            <canvas className="tunnel" id="tunnel-3"></canvas>
+            
           </Card>
-        </Col>
+        </Col> */}
       </Row>
       <Row style={{marginBottom: 24}}>
         <Col span={24}>
           <Card title="流量整合图">
-            <div style={{height: 300}}></div>
+            <canvas className="tunnel" id="tunnel-1"></canvas>
           </Card>
         </Col>
       </Row>
       <Card title="视频播放列表">
-        <Row gutter={24} style={{marginBottom: 24}}>
+        <Row gutter={24} style={{marginBottom: 24}} className="video-box">
           <Col span={8}>
             <video controls src="http://localhost:8000/path/path1/stream.m3u8"></video>
           </Col>
