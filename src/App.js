1caeda9145e2e24a01ff814975157865b2e08974
liyizhi <tgxpuisb@hotmail.com>
2024-08-08 09:30:19 +0800

    api concent

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import './App.css';
 
 function App() {
 
-  const [infos, setInfos] = useState([100, 20, 40, 50])
+  const [infos, setInfos] = useState(null)
 
   useEffect(() => {
     axios
       .get('http://localhost:8001/parameters')
       .then(res => {
         console.log(res)
+        if (res.status === 200) {
+          setInfos(res.data)
+        }
       })
       .catch(e => {
         console.log(e)
@@ -77,12 +80,12 @@ function App() {
       <Row gutter={24} style={{marginBottom: 24}}>
         <Col span={24}>
           <Card title="流量信息栏">
-            {infos.length > 0 ? 
+            {infos ? 
               <>
-                <div>隧道内车辆总数：<b>{infos[0]}</b></div>
-                <div>隧道内第一段车辆数：<b>{infos[1]}</b></div>
-                <div>隧道内第二段车辆数：<b>{infos[2]}</b></div>
-                <div>每小时车流量：<b>{infos[3]}</b></div>
+                <div>隧道内车辆总数：<b>{infos.parameter1}</b></div>
+                <div>隧道内第一段车辆数：<b>{infos.parameter2}</b></div>
+                <div>隧道内第二段车辆数：<b>{infos.parameter3}</b></div>
+                <div>每小时车流量：<b>{infos.parameter4}</b></div>
               </>:
               <span>暂无数据</span>
             }
@@ -105,13 +108,13 @@ function App() {
       <Card title="视频播放列表">
         <Row gutter={24} style={{marginBottom: 24}} className="video-box">
           <Col span={8}>
-            <video controls src="http://localhost:8000/path/path1/stream.m3u8"></video>
+            <video controls src="http://localhost:8001/path/path1/stream.m3u8"></video>
           </Col>
           <Col span={8}>
-            <video controls src="http://localhost:8000/path/path2/stream.m3u8"></video>
+            <video controls src="http://localhost:8001/path/path2/stream.m3u8"></video>
           </Col>
           <Col span={8}>
-            <video controls src="http://localhost:8000/path/path3/stream.m3u8"></video>
+            <video controls src="http://localhost:8001/path/path3/stream.m3u8"></video>
           </Col>
         </Row>
       </Card>
